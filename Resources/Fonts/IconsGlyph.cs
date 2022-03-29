7745e925b217801cf70b79680614e34974018cbd
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2022-03-29 12:09:39 +0200

    Add Icons Font

---
+++ Resources/Fonts/IconsGlyph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Gameball.Resources.Fonts;
+
+public static class IconsGlyph
+{
+	public static readonly string FullStar = "\ue900";
+	public static readonly string Circle = "\ue904";
+	public static readonly string Rollback = "\ue905";
+	public static readonly string Clone = "\ue906";
+	public static readonly string Facebook = "\ue907";
+	public static readonly string Star = "\ue90c";
+	public static readonly string Twitter = "\ue90d";
+}
+
